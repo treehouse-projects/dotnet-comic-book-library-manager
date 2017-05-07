dfe486ca0f33cc9a57ea21c19ac5a22a7ef92d15
James Churchill <james@smashdev.com>
2017-05-07 15:28:13 -0700

    Implemented deletes in the ComicBooksController class

--- src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ src/ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -146,9 +146,10 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            // TODO Get the comic book.
-            // Include the "Series" navigation property.
-            var comicBook = new ComicBook();
+            var comicBook = _context.ComicBooks
+                .Include(cb => cb.Series)
+                .Where(cb => cb.Id == id)
+                .SingleOrDefault();
 
             if (comicBook == null)
             {
@@ -161,7 +162,9 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         [HttpPost]
         public ActionResult Delete(int id)
         {
-            // TODO Delete the comic book.
+            var comicBook = new ComicBook() { Id = id };
+            _context.Entry(comicBook).State = EntityState.Deleted;
+            _context.SaveChanges();
 
             TempData["Message"] = "Your comic book was successfully deleted!";
 
