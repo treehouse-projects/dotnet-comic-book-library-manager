5e8673d66baff4a95a911ba025fcbf8c515761cd
James Churchill <james@smashdev.com>
2017-05-10 13:04:18 -0700

    Added the BaseRepository generic base class

---
+++ src/ComicBookShared/Data/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ComicBookShared.Data
+{
+    public abstract class BaseRepository<TEntity>
+        where TEntity : class
+    {
+        protected Context Context { get; private set; }
+
+        public BaseRepository(Context context)
+        {
+            Context = context;
+        }
+
+        public abstract TEntity Get(int id, bool includeRelatedEntities = true);
+        public abstract IList<TEntity> GetList();
+
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var set = Context.Set<TEntity>();
+            var entity = set.Find(id);
+            set.Remove(entity);
+            Context.SaveChanges();
+        }
+    }
+}
